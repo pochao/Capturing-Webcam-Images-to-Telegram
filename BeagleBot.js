3ee7e6c4fc83d9212f5e7896c98559a77260ab69
pochao <pochao.huang@hotmail.com>
2017-02-20 00:31:12 +0800

    Create BeagleBot.js

---
+++ BeagleBot.js
@@ -0,0 +1,48 @@
+var telegram = require('telegram-bot-api')
+
+var api = new telegram({
+    token: '321457475:AAH5QiqvdI3JKkBFFmwXdNQOliXHwAEGdU8',
+    updates: {
+        enabled: true
+    }
+})
+
+api.getMe().then(function(data){
+        console.log('telegram bot connected')
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+    
+api.on('message', function(message) {
+    if (message.text.substring(0, 4).toLowerCase() == 'take') {
+        var exec = require('child_process').exec
+        var filename = "webcam.py"
+        exec("python" + " " + filename,function(err,stdout,stderr){
+            if(err)
+            {
+                console.log(err)
+            }
+            if(stdout)
+            {
+                console.log(stdout)
+                /*
+                api.sendMessage({
+                    chat_id: message.chat.id,
+                    text: stdout
+                })
+                */
+                api.sendPhoto({
+                    chat_id: message.chat.id,
+                    caption: 'webcam image',
+                    // you can also send file_id here as string (as described in telegram bot api documentation)
+                    photo: './img.png'
+                })
+                .then(function(data)
+                {
+                    //console.log(util.inspect(data, false, null));
+                })
+            }
+        })
+    }
+})
